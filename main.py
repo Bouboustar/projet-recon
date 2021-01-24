e0b2e81ae0051163915dbd994364c29f1a0afddc
Bouboustar <63979093+Bouboustar@users.noreply.github.com>
2021-01-24 14:26:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,12 @@
 
 #Main program
 def main():
-    print("Hello World !")
-
+    choix = int(input("Choissisez 1. Pour l'enregistrement 2. Pour la reconnaissance du visage"))
+    if choix == 1 :
+        print("Bienvenue dans le mode enregistrement")
+    if choix == 2 :
+        print("Bienvenue dans le mode reconnaissance")
+    else : 
+        choix = int(input("Choissisez 1. Pour l'enregistrement 2. Pour la reconnaissance du visage"))
 if __name__ == "__main__":
     main()
