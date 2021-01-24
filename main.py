bc1d0ee131cf0afd2fe11f765714135c83625566
Bouboustar <63979093+Bouboustar@users.noreply.github.com>
2021-01-24 14:29:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@
 
 #Main program
 def main():
-    choix = int(input("Choissisez 1. Pour l'enregistrement 2. Pour la reconnaissance du visage"))
+    choix = int(input("Choissisez 1. Pour l'enregistrement 2. Pour la reconnaissance du visage : "))
     if choix == 1 :
         print("Bienvenue dans le mode enregistrement")
-    if choix == 2 :
+    elif choix == 2 :
         print("Bienvenue dans le mode reconnaissance")
-    else : 
-        choix = int(input("Choissisez 1. Pour l'enregistrement 2. Pour la reconnaissance du visage"))
+    elif choix != 1 and choix != 2: 
+        choix = int(input("Choissisez 1. Pour l'enregistrement 2. Pour la reconnaissance du visage : "))
 if __name__ == "__main__":
     main()
