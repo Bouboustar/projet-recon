ce594f89b62da3d0ea7992d206dfd2afd96c93cb
Bouboustar <63979093+Bouboustar@users.noreply.github.com>
2021-01-24 15:30:53 +0100

    Update detection.py

--- detection.py
+++ detection.py
@@ -5,32 +5,32 @@ import cv2
 import sys
 
 
-
-
 # Programme
-cascPath = r'./opencv-xml/haarcascade_frontalface_default.xml'
-faceCascade = cv2.CascadeClassifier(cascPath)
 
-video_capture = cv2.VideoCapture(0)
+def detection():
+    cascPath = r'./opencv-xml/haarcascade_frontalface_default.xml'
+    faceCascade = cv2.CascadeClassifier(cascPath)
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
+    video_capture = cv2.VideoCapture(0)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-# Arret de la capture
-return faces
-video_capture.release()
-cv2.destroyAllWindows()
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Arret de la capture
+    return faces
+    video_capture.release()
+    cv2.destroyAllWindows()
