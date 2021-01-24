0ba889f9f158ccd49da5683e9a131199c759078b
David-Tisseraud <77575292+David-Tisseraud@users.noreply.github.com>
2021-01-24 14:56:39 +0100

    main.py

--- main.py
+++ main.py
@@ -4,12 +4,38 @@
 
 #Main program
 def main():
+    
     choix = int(input("Choissisez 1. Pour l'enregistrement 2. Pour la reconnaissance du visage : "))
+    
     if choix == 1 :
         print("Bienvenue dans le mode enregistrement")
+        
+        
+        
+        
+        
+        
+        
+        
     elif choix == 2 :
         print("Bienvenue dans le mode reconnaissance")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     elif choix != 1 and choix != 2: 
         choix = int(input("Choissisez 1. Pour l'enregistrement 2. Pour la reconnaissance du visage : "))
+        
+        
 if __name__ == "__main__":
     main()
